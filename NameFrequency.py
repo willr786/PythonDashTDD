460b32fade3d0d891715d2ae6b11dc6e83bdec99
willr786 <willr_786@yahoo.com>
2021-02-27 18:51:28 -0500

    Finish part 2

--- NameFrequency.py
+++ NameFrequency.py
@@ -18,7 +18,7 @@ class NameFrequency:
     def preparingData(self, file_name, column_name):
         data = pd.read_csv(file_name)
         data.dropna(inplace=True)
-        data_frame = dict(data[column_name].str.split(" ", n=1, expand=True))
+        data_frame = pd.DataFrame(dict(data[column_name].str.split(" ", n=1, expand=True)))
         self.data_frame = data_frame
         if isinstance(self.data_frame, pd.DataFrame):
             return True
